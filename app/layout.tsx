e30a7757183e696051f1a9768ced2337bb81f274
Mick Wollman <mick.wollman@gmail.com>
2023-08-21 12:29:50 -0400

    Initial Commit.

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { UIProvider } from 'context/chakraProvider';
+
+//TODO(MW): Seems like this should be in page.js, but can't go there until it's converted to a server component.
+export const metadata = {
+    title: 'Natural Time',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang='en'>
+            <body>
+                  <UIProvider>
+                      {children}
+                  </UIProvider>
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import {
+    Text,
+} from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <Text>This is a website template!</Text>
+    );
+}
